483db26ebc4a16eb9d2e8f81947f4ba43eeb797d
Michal <michal.sobiech15@gmail.com>
2023-08-30 23:19:03 +0200

    Refactored joystick code

--- src/joystick/Joystick.cpp
+++ src/joystick/Joystick.cpp
@@ -12,13 +12,9 @@ Joystick::Joystick(AdcGpio x_axis_adc_gpio, AdcGpio y_axis_adc_gpio) {
 
 std::pair<unsigned int, unsigned int> Joystick::get_joystick_position() {
     // Read X
-    adc_gpio_init(x_axis_adc_gpio_.get_adc_no());
-    adc_select_input(x_axis_adc_gpio_.get_gpio_no());
-    unsigned int x_value = adc_read();
+    unsigned int x_value = x_axis_adc_gpio_.read();
     // Read Y
-    adc_gpio_init(y_axis_adc_gpio_.get_adc_no());
-    adc_select_input(y_axis_adc_gpio_.get_gpio_no());
-    unsigned int y_value = adc_read();
+    unsigned int y_value = y_axis_adc_gpio_.read();
 
     return std::make_pair(x_value, y_value);
 }
