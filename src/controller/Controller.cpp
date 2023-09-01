273450bcc9281f548b1920f352430bd4e204fa2d
Michal <michal.sobiech15@gmail.com>
2023-09-01 13:27:38 +0200

    Controller SoC meter commit 2

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -2,15 +2,21 @@
 #include "NRF24L01PlusTX.hpp"
 #include "Joystick.hpp"
 #include "AdcGpio.hpp"
+#include "ControllerSocMeter.hpp"
 
 Controller::Controller() {
     transceiver_ = NRF24L01PlusTX();
     joystick_ = Joystick(AdcGpio(26, 0), AdcGpio(27, 1));
+    soc_meter_ = ControllerSocMeter();
 }
 
 NRF24L01PlusTX& Controller::get_transceiver() {
     return transceiver_;
 };
+
+ControllerSocMeter& Controller::get_soc_meter() {
+    return soc_meter_;
+}
 
 std::pair<unsigned int, unsigned int> Controller::get_joystick_position() {
     return joystick_.get_joystick_position();
