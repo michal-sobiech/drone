0d950d98c0a75b0d682b82a1e4c346276d70d802
Michal <michal.sobiech15@gmail.com>
2023-09-01 13:19:23 +0200

    uint changed to unsigned int in AdcGpio

--- include/adc_gpio/AdcGpio.hpp
+++ include/adc_gpio/AdcGpio.hpp
@@ -6,7 +6,7 @@ public:
     AdcGpio(unsigned int adc_no, unsigned int gpio_no);
     unsigned int get_adc_no();
     unsigned int get_gpio_no();
-    uint read();
+    unsigned int read();
 private:
     unsigned int adc_no_;
     unsigned int gpio_no_;
