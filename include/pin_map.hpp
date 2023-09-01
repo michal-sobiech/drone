b08e467840a9cd6af052e70ec8213715059bc928
Michal <michal.sobiech15@gmail.com>
2023-09-01 14:47:32 +0200

    Added a map of used pins

---
+++ include/pin_map.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+// +-------------------+
+// |       Drone       |
+// +-------------------+
+
+// #define 
+
+// +-------------------+
+// |    Controller     |
+// +-------------------+
+
+// SoC meter
+#define SOC_METER_GPIO_NO 26
+#define SOC_METER_ADC_NO 0
+
+// Joysticks
+#define JOYSTICK_GPIO_NO 27
+#define JOYSTICK_ADC_NO 1
+#define JOYSTICK_MUX_SEL_H_GPIO_NO 21
+#define JOYSTICK_MUX_SEL_L_GPIO_NO 20
