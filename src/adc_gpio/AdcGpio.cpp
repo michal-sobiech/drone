03fbb12ca7764108e48950e5f7ba95ee8a6f81ba
Michal <michal.sobiech15@gmail.com>
2023-09-01 13:18:53 +0200

    AdcGpio initializer list

--- src/adc_gpio/AdcGpio.cpp
+++ src/adc_gpio/AdcGpio.cpp
@@ -1,11 +1,10 @@
 #include "AdcGpio.hpp"
+#include "hardware/adc.h"
 
 AdcGpio::AdcGpio() {}
 
-AdcGpio::AdcGpio(unsigned int adc_no, unsigned int gpio_no) {
-    adc_no_ = adc_no;
-    gpio_no = gpio_no;
-}
+AdcGpio::AdcGpio(unsigned int adc_no, unsigned int gpio_no):
+    adc_no_(adc_no), gpio_no_(gpio_no) {}
 
 unsigned int AdcGpio::get_adc_no() {
     return adc_no_;
@@ -15,7 +14,7 @@ unsigned int AdcGpio::get_gpio_no() {
     return gpio_no_;
 }
 
-uint AdcGpio::read() {
+unsigned int AdcGpio::read() {
     adc_gpio_init(get_adc_no());
     adc_select_input(get_gpio_no());
     return adc_read();
