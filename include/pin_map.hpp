e2adc64bca0a19c0d108d917af9780e4618bd383
Michal <michal.sobiech15@gmail.com>
2023-09-01 14:54:57 +0200

    Updated the pin map

--- include/pin_map.hpp
+++ include/pin_map.hpp
@@ -4,18 +4,22 @@
 // |       Drone       |
 // +-------------------+
 
-// #define 
+// SoC meter and its mux
+#define DRONE_SOC_METER_GPIO_NO 26
+#define DRONE_SOC_METER_ADC_NO 0
+#define DRONE_SOC_MUX_SEL_H_GPIO_NO 21
+#define DRONE_SOC_MUX_SEL_L_GPIO_NO 20
 
 // +-------------------+
 // |    Controller     |
 // +-------------------+
 
-// SoC meter
-#define SOC_METER_GPIO_NO 26
-#define SOC_METER_ADC_NO 0
-
-// Joysticks
-#define JOYSTICK_GPIO_NO 27
-#define JOYSTICK_ADC_NO 1
+// Joysticks and their mux
+#define JOYSTICK_GPIO_NO 26
+#define JOYSTICK_ADC_NO 0
 #define JOYSTICK_MUX_SEL_H_GPIO_NO 21
 #define JOYSTICK_MUX_SEL_L_GPIO_NO 20
+
+// SoC meter
+#define CONTROLLER_SOC_METER_GPIO_NO 27
+#define CONTROLLER_SOC_METER_ADC_NO 1
