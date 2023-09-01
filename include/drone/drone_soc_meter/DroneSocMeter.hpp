be3889a7dad0f8aece2496f8f073aac364364de3
Michal <michal.sobiech15@gmail.com>
2023-09-01 15:01:57 +0200

    Added the drone SoC meter

---
+++ include/drone/drone_soc_meter/DroneSocMeter.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "FourChannelMux.hpp"
+#include "CellSocMeter.hpp"
+
+#define DRONE_BAT_CELL_MIN_V 3400
+#define DRONE_BAT_CELL_MAX_V 4200
+
+class DroneSocMeter() {
+public:
+    DroneSocMeter();
+
+    // Fills the array with the percentages
+    void get_cells_soc(unsigned int cells_soc[]);
+private:
+    FourChannelMux mux_;
+
+    CellSocMeter cell_soc_meter;
+
+    // Fills the array with the voltages in mV
+    void get_cells_voltage(unsigned int voltages[]);
+
+    // Set the multiplexer to given cell and read its voltage in mV.
+    // Uses a voltage divider.
+    // cell_no is 0, 1, 2 or 3
+    unsigned int DroneSocMeter::get_cell_voltage(unsigned int cell_no);
+};
---
+++ src/drone/drone_soc_meter/DroneSocMeter.cpp
@@ -0,0 +1,28 @@
+#include "DroneSocMeter.hpp"
+#include "pin_map.hpp"
+#include "FourChannelMux.hpp"
+#include "CellSocMeter.hpp"
+
+DroneSocMeter::DroneSocMeter() {
+    mux_ = FourChannelMux(
+        {DRONE_SOC_METER_MUX_SEL_H_GPIO_NO,
+        DRONE_SOC_METER_MUX_SEL_L_GPIO_NO}
+    );
+
+    cell_soc_meter = CellSocMeter(
+        DRONE_BAT_CELL_MIN_V,
+        DRONE_BAT_CELL_MAX_V,
+        AdcGpio(DRONE_SOC_METER_ADC_NO, DRONE_SOC_METER_GPIO_NO)
+    );
+}
+
+void DroneSocMeter::get_cells_soc(unsigned int cells_soc[]) {
+    for (unsigned int i = 0; i < 4; i++) {
+        cells_soc[i] = get_cell_soc(i);
+    }
+}
+
+unsigned int DroneSocMeter::get_cell_soc(unsigned int cell_no) {
+    mux_.choose_channel(cell_no);
+    return cell_soc_meter_.get_soc();
+}
