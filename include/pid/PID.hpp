09fb9f28a3f3d7e781ef4a0d3d7892505dc3c183
Michał Sobiech <place@holder>
2024-02-12 12:18:24 +0100

    Made getters and setters public

--- include/pid/PID.hpp
+++ include/pid/PID.hpp
@@ -6,6 +6,12 @@ public:
     PID();
     // Calculates the control output
     float calc_u(float y_sp, float prev_y);
+    void set_k_p(float k_p);
+    void set_k_i(float k_i);
+    void set_k_d(float k_d);
+    float get_k_p();
+    float get_k_i();
+    float get_k_d();
 protected:
     float _k_p;
     float _k_i;
@@ -13,11 +19,4 @@ protected:
     float _last_error;
     float _integral;
     float _dt;
-
-    void set_k_p(float k_p);
-    void set_k_i(float k_i);
-    void set_k_d(float k_d);
-    float get_k_p();
-    float get_k_i();
-    float get_k_d();
 };
