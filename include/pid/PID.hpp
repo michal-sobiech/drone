682786436862996629b17b3ec691dfd41a8a062b
Michał Sobiech <place@holder>
2024-02-12 12:17:53 +0100

    setters and getters for PID

--- include/pid/PID.hpp
+++ include/pid/PID.hpp
@@ -13,4 +13,11 @@ protected:
     float _last_error;
     float _integral;
     float _dt;
+
+    void set_k_p(float k_p);
+    void set_k_i(float k_i);
+    void set_k_d(float k_d);
+    float get_k_p();
+    float get_k_i();
+    float get_k_d();
 };
--- src/pid/PID.cpp
+++ src/pid/PID.cpp
@@ -18,3 +18,33 @@ float PID::calc_u(float y_sp, float prev_y) {
 
     return p_term + i_term + d_term;
 };
+
+
+void PID::set_k_p(float k_p) {
+    _k_p = k_p;
+}
+
+
+void PID::set_k_i(float k_i) {
+    _k_i = k_i;
+}
+
+
+void PID::set_k_d(float k_d) {
+    _k_d = k_d;
+}
+
+
+float PID::get_k_p() {
+    return _k_p;
+}
+
+
+float PID::get_k_i() {
+    return _k_i;
+}
+
+
+float PID::get_k_d() {
+    return _k_d;
+}
