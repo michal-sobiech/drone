86c1bfa32775d422d173928527ed719673ad0d85
Michał Sobiech <place@holder>
2024-02-14 23:48:21 +0100

    EngineSpeeds::operator+

--- include/drone/engine/EngineSpeeds.hpp
+++ include/drone/engine/EngineSpeeds.hpp
@@ -6,4 +6,13 @@ struct EngineSpeeds {
     float fl_speed;
     float bl_speed;
     float br_speed;
+
+    EngineSpeeds operator+(const EngineSpeeds& speeds) const {
+        return EngineSpeeds{
+            fr_speed + speeds.fr_speed,
+            fl_speed + speeds.fl_speed,
+            bl_speed + speeds.bl_speed,
+            br_speed + speeds.br_speed
+        };
+    }
 };
